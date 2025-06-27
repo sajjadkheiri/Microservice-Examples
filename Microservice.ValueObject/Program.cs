f68f5f4b7ba874c32f8167a60a9ef517ac7f1455
sajjad <sajjadkheiri7@gmail.com>
2025-06-27 15:02:58 +0330

    Initial to concentrate on Value object

---
+++ Microservice.ValueObject/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microservice.ValueObject.Model;
+
+var oneMeter01 = new Meter(1);
+var oneMeter02 = new Meter(1);
+var twoMeter01 = new Meter(2);
+
+var oneMeter = oneMeter01 == oneMeter02;
+var twoMeter = oneMeter01 == twoMeter01;
+
+Console.WriteLine($"OneMeter: {oneMeter}");
+Console.WriteLine($"TwoMeter: {twoMeter}");
+
+Console.ReadKey();
